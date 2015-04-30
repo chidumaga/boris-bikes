b884df242dde4f36887c468a53f8b2e7418ed8a3
Makers Laptop 66 <makerslaptop66@Makerss-MBP-4.home>
2015-05-01 00:10:04 +0100

    modified unit tests and feature tests, all tests pass now

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,12 @@
 class DockingStation
+	
 	def release_bike
-   	Bike.new
+		fail "No bikes available" unless @bike
+		@bike
 	end
+
+	def dock bike
+		@bike = bike
+	end
+
 end
--- spec/features/boris_bikes_features_spec.rb
+++ spec/features/boris_bikes_features_spec.rb
@@ -2,8 +2,15 @@ require 'docking_station'
 
 feature "User can get a working bike from docking station" do 
 	scenario "A working bike is released from the docking station at the user's request" do 
-		station = DockingStation.new #We create a new docking station that we can work with
-		bike = station.release_bike #the docking station releases a bike
-		expect(bike).to be_working #the bike must be working
+		station = DockingStation.new
+		station.dock Bike.new
+		bike = station.release_bike
+		expect(bike).to be_working
+	end
+
+	scenario "Docking Station to not release bike when there are no bikes available" do 
+		station = DockingStation.new 
+		expect{ station.release_bike }.to raise_error "No bikes available"
 	end
 end
+
