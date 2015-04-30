ce53bc4fe16df0c21cb11ebc004d1f6e043fe674
Makers Laptop 66 <makerslaptop66@Makerss-MacBook-Pro-2.local>
2015-04-30 14:30:18 +0100

    Wrote feature tests and unit tests, got all tests to pass

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,7 +4,17 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it "releases working bikes" do
+  	subject.dock Bike.new
   	bike = subject.release_bike
   	expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  describe 'release bike' do
+  	it 'raises and error when there are no bikes available' do
+  		expect { subject.release_bike }.to raise_error 'No bikes available'
+  	end	
+  end
+
 end
--- spec/feature_test_boris_spec.rb
+++ spec/feature_test_boris_spec.rb
@@ -1,10 +1,16 @@
 require 'dockingstation'
 
-
 feature "Docking station releases bikes" do
 	scenario "Bike is released to user from docking station" do
-		station = DockingStation.new
-		bike = station.release_bike
-		expect(bike).to be_working
+		station = DockingStation.new   # build a new docuking station, its empty
+		station.dock Bike.new  #add a bike to the new docking station
+		bike = station.release_bike  #now there is a bike to release so we release it.
+		expect(bike).to be_working  #the bike thats been released is working
+	end
+
+	scenario "Docking station does not release a bike when there are none available" do 
+		docking_station = DockingStation.new #build a new docking station
+		expect { docking_station.release_bike }.to raise_error 'No bikes available' #the code in between the curly braces execute but are stopped by rspec from crashing the program
+																					# there are no bikes in the docking station when we try and get one, so throw an error
 	end
 end
